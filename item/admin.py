1ff4c1cd80b9971bff7be30ea6f1317ebb863891
razeevnepali <rajeev.nepali1001@gmail.com>
2019-09-16 13:12:33 +0545

    Updated Image field

--- item/admin.py
+++ item/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 from . models import HomepageImage, Organization, SolutionPage, ProcessPage, ProcessStep, Service, Project, ProjectImage, AboutUs, ContactDetail, Message, SolutionWeProvide
 
 # Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 2
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ ProjectImageInline, ]
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(HomepageImage)
 admin.site.register(Organization)
 admin.site.register(SolutionPage)
 admin.site.register(ProcessPage)
 admin.site.register(ProcessStep)
 admin.site.register(Service)
-admin.site.register(Project)
-admin.site.register(ProjectImage)
 admin.site.register(AboutUs)
 admin.site.register(ContactDetail)
 admin.site.register(Message)
